8453611bf000d84303b4d6d854175e775bad25b6
Johann <johnhansong@gmail.com>
2023-08-31 21:32:08 -0700

    jsong-my-profile-edits

---
+++ my-profile.js
@@ -0,0 +1,19 @@
+// export const myName = () => {
+//     const h1 = document.createElement("h1");
+//     h1.id = "name";
+//     h1.innerText = "John";
+//     h1.className = "name";
+// }
+
+// window.onload = () => {
+    // myName();
+        // const h1 = document.createElement("h1");
+        // h1.id = "name";
+        // h1.innerText = "John";
+        // h1.className = "name";
+
+// }
+window.addEventListener("DOMContentLoaded", event => {
+    const h1 = document.createElement("h1");
+    h1.innerText = "John"
+})
